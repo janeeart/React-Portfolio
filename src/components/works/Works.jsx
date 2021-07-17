6eab5bb0e5f698aecaf1bc65ef344f2f95e4cc25
Janée Artis <janeeartis@gmail.com>
2021-07-17 09:06:56 -0600

    Polish works section + increase mobile compatibilities

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -10,7 +10,9 @@ export default function Works() {
             icon: "./assets/music.png",
             title: "Artist Master",
             desc: "A web application for music lovers to find information, including biography and upcoming events, for a chosen music artist.",
-            img: "./assets/artistmaster.png"
+            img: "./assets/artistmaster.png",
+            deployed: "https://edodgion.github.io/Project-1-Global-Music/",
+            github: "https://github.com/janeeart/Project-1-Global-Music"
         },
     
         {
@@ -18,41 +20,34 @@ export default function Works() {
             icon: "./assets/drink.png",
             title: "Five O'Clock Somewhere",
             desc: "A web application for adult beverage connoisseurs who want to find and save cocktail recipes by searching for ingredients or cocktail names.",
-            img: "./assets/5oclock.png"
+            img: "./assets/5oclock.png",
+            deployed:"https://pure-plains-23888.herokuapp.com/",
+            github: "https://github.com/janeeart/5-0-Clock-Drink-Generator"
+
         },
         {
             id: 3,
-            icon: "./assets/mobile.png",
-            title: "Project 3",
-            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium velit aperiam sapiente nesciunt ducimus consectetur, sed ut tempora nulla in incidunt architecto? Illo, eveniet. Nobis non omnis corrupti hic temporibus?",
-            img: "./assets/website.jpg"
-        },
-        {
-            id: 4,
             icon: "./assets/weathericon.png",
             title: "Weather Dashboard",
             desc: "This web application is a weather dashboard that features dynamically updated HTML and CSS. This web app uses OpenWeather API to retrieve weather data for cities and local storage to store persistent data.",
-            img: "./assets/weather.png"
+            img: "./assets/weather.png",
+            deployed: "https://janeeart.github.io/Server-Side-APIs-Weather-Dashboard-HW-6/",
+            github: "https://github.com/janeeart/Server-Side-APIs-Weather-Dashboard-HW-6"
         },
         {
-            id: 5,
+            id: 4,
             icon: "./assets/teamicon.png",
             title: "Team Profile Generator",
             desc: "This is a Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.",
-            img: "./assets/teamprofile.png"
-        },
-        {
-            id: 6,
-            icon: "./assets/globe.png",
-            title: "Placeholder",
-            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium velit aperiam sapiente nesciunt ducimus consectetur, sed ut tempora nulla in incidunt architecto? Illo, eveniet. Nobis non omnis corrupti hic temporibus?",
-            img: "./assets/website.jpg"
+            img: "./assets/teamprofile.png",
+            deployed: "https://drive.google.com/file/d/1iRAx2_duwoUUXHs2DOWCWXFJJnDjmQ9M/view",
+            github: "https://github.com/janeeart/Object-Oriented-Programming-Team-Profile-Generator-HW-10"
         }
     ];
 
     const handleClick = (way) => {
         way === "left" 
-        ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 5) :
+        ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 3) :
         setCurrentSlide(currentSlide  < data.length -1 ? currentSlide + 1 : 0 )
     }
         
@@ -74,7 +69,8 @@ export default function Works() {
     
                                 <h2>{d.title}</h2>
                                 <p>{d.desc}</p>
-                            <span>Deployed Project</span>
+                            <span><a href={d.deployed}>Deployed Project</a></span>
+                            <span><a href={d.github}>GitHub Repository</a></span>
                             </div>
                         </div>
                         <div className="right">
