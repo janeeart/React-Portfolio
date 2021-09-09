84b32914ef8cb10d24ac126bfb21d2b6a98443f3
Janée Artis <janeeartis@gmail.com>
2021-09-08 18:57:10 -0600

    Contact Me form works

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,25 @@
 import "./contact.scss"
 import {useState} from "react";
+import{ init} from 'emailjs-com';
+import emailjs from 'emailjs-com';
+init("user_oS91syJxkTS29MS4NuAyQ");
 
 export default function Contact() {
         const [message, setMessage] = useState(false)
-    const handleSubmit = (e) => {
+    function handleSubmit(e) {
         e.preventDefault();
         setMessage(true)
+        
+            emailjs.sendForm('service_wddg5xl', 'template_c1jkh3a', e.target, 'user_oS91syJxkTS29MS4NuAyQ')
+              .then((result) => {
+                  console.log(result.text);
+              }, (error) => {
+                  console.log(error.text);
+              });
+          e.target.reset()
     }
+
+    
      return (
         <div className="contact" id="contact">
             <div className="left">
@@ -15,8 +28,8 @@ export default function Contact() {
                 <div className="right">
                     <h2>Get In Touch.</h2>
                     <form onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Email"/>
-                        <textarea placeholder="Message"></textarea>
+                        <input name="email" placeholder="Email"/>
+                        <textarea name="message" placeholder="Message"></textarea>
                         <button type="submit">Send</button>
                         {message && <span>Thank you! I'll reply ASAP :)</span>}
                     </form>
